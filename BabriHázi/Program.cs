870556989e3f6eaaaa01bfd21b2508ed680a699e
JohnFury77 <furijani99@gmail.com>
2020-09-30 20:44:28 +0200

    Add project files.

---
+++ BabriHázi/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BabriHázi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int szam = int.Parse(Console.ReadLine());   
+            int összeg = 0;
+
+            for (int i = 1; i <= szam; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    if (szam % j == 0)
+                    {
+                        összeg += j;
+                    }
+                }
+            }
+            
+
+            Console.WriteLine($"1-{szam}-ig a számok osztóinak összege: {összeg}");
+            Console.ReadLine();
+
+
+        }
+    }
+}
